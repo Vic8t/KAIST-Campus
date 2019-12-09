2188fb11df3ce94dfd4a89191e1462e875b104ba
readleyj <readleyj@gmail.com>
2019-12-09 16:32:28 +0900

    Added GUI

---
+++ app/src/main/java/com/example/kaistcampusv2/ui/map/MapViewModel.java
@@ -0,0 +1,12 @@
+package com.example.kaistcampusv2.ui.map;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MapViewModel extends ViewModel {
+
+    public MapViewModel() {
+
+    }
+}
