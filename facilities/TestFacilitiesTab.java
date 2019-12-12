cd99a69df33cd10b7245172bce5572de9746ba01
Victor <vic8.taillieu@gmail.com>
2019-12-12 17:32:27 +0900

    Deleted useless files

--- facilities/TestFacilitiesTab.java
+++
@@ -1,9 +0,0 @@
-public class TestFacilitiesTab {
-    public static void main(String[] args) {
-
-        FacilitiesTab facilitiesTab = new FacilitiesTab();
-        facilitiesTab.loadData("facilities.csv");
-
-        facilitiesTab.displayOnCampusFacilities();
-    }
-}
